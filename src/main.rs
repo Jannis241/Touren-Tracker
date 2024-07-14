8d07a7c2f7af64aded48fd8f651710e4cf582176
Maus <maus@sleepy.home>
2024-07-14 21:37:02 +0200

    renamed main file

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,88 @@
-
-
-
-
-use druid::{AppLauncher, Color, Widget, WindowDesc};
-use druid::widget::{Label, Flex, Button};
-
-fn build_ui() -> impl Widget<()> {
-    let label = Label::new("Hello, Druid!");
-    let button = Button::new("Click me").on_click(|_ctx, _data, _env| {
-        println!("Button clicked!");
-    });
-    Flex::column()
-        .with_child(label)
-        .with_child(button)
+enum Tour_type {
+    Leicht,
+    Medium,
+    Monster,
+    Zerstörer,
+}
+#[derive(PartialEq)]
+enum Person {
+    Mama,
+    Papa,
+    Jesko,
+    Jannis,
 }
 
-fn main() {
-    let main_window = WindowDesc::new(build_ui())
-        .title("Druid Example")
-        .window_size((400.0, 400.0));
-    AppLauncher::with_window(main_window)
-        .launch(())
-        .expect("Failed to launch application");
+struct Tour {
+    name: String,
+    kilometer: f32,
+    höhenmeter: i32,
+    tour_type: Option<Tour_type>,
+    teilnehmer: Vec<Person>,
+    schwierigkeitsBonus: i32,
+}
+
+
+fn writeToJSON(tour: &Tour){
+
+}
+
+pub impl Tour {
+    pub fn new(name: &str, kilometer: f32, höhenmeter: i32, teilnehmer: Vec<Person>, schwierigkeitsBonus: i32) -> Tour{
+        let name = name.to_string();
+        let mut tour_type: Option<Tour_type> = None;
+        if höhenmeter >= 0 && höhenmeter <= 499 {
+            tour_type = Some(Tour_type::Leicht);
+        }
+        if höhenmeter >= 500 && höhenmeter <= 999 {
+            tour_type = Some(Tour_type::Medium);
+        }
+        if höhenmeter >= 1000 && höhenmeter <= 1399 {
+            tour_type = Some(Tour_type::Monster);
+        }
+        if höhenmeter >= 1400{
+            tour_type = Some(Tour_type::Zerstörer);
+        }
+        
+        Tour {
+            name,
+            kilometer,
+            höhenmeter,
+            tour_type,
+            teilnehmer,
+            schwierigkeitsBonus,
+        }
+    }
+
+    pub fn calculateMoney(&self, person: Person) -> i32{
+        let mut money = 0;
+        if self.teilnehmer.contains(&person) {
+            money += self.schwierigkeitsBonus;
+
+            // tour type money
+            match self.tour_type{
+                Some(Tour_type::Medium)=> money += 3,
+                Some(Tour_type::Monster) => money += 5,
+                Some(Tour_type::Zerstörer) => money += 10,
+                _ => println!("Tour type not found.."),
+            }
+    
+            if self.kilometer >= 10.0 {
+                money += 1;
+            }
+    
+            if self.kilometer >= 15.0 {
+                money += 1
+            }  
+        }
+
+        money   
+    }
+}
+
+
+fn main(){
+    let neueTour = Tour::new("test", 9.0, 500, vec![Person::Jannis, Person::Jesko], 0);
+    let money = neueTour.calculateMoney(Person::Jesko);
+
+    println!("{:?}", money);
 }
