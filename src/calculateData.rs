cd7a4baaace331a39d619630ba63f8e2cc197ece
Maus <maus@sleepy.home>
2024-07-14 20:05:16 +0200

    test

--- src/calculateData.rs
+++ src/calculateData.rs
@@ -1,3 +1,3 @@
-fn krombola {
-    
+fn krombolaKrabbe {
+
 }
--- uploadFeature.py
+++ uploadFeature.py
@@ -12,29 +12,11 @@ def run_command(command):
 
 def main():
     try:
-        # Stash local changes to apply after merge
-        print("Stashing local changes...")
-        run_command("git stash")
-
-        # Pull the latest changes, automatically merging
-        print("Pulling the latest changes from the remote repository...")
-        run_command("git pull")
-
-        # Apply stashed changes, if any
-        print("Applying stashed local changes...")
-        run_command("git stash pop")
-
-        # Add all changes to staging
-        print("Adding all local changes to staging...")
-        run_command("git add .")
-
-        # Commit the changes
-        commit_message = "Auto-commit local changes"
+        commit_message=input("commit msg: ")
         print(f"Committing changes with message: '{commit_message}'")
-        run_command(f"git commit -m '{commit_message}'")
 
-        # Push the changes to the remote repository
-        print("Pushing changes to the remote repository...")
+        run_command(f'git commit -a -m "{commit_message}"')
+        run_command("git pull --rebase origin main")
         run_command("git push")
 
         print("All changes have been successfully pushed!")
