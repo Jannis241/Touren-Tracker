a96d4e986edaf9b5ebe58bafd97ba0fdf51fd7f1
Maus <maus@sleepy.home>
2024-07-14 21:29:10 +0200

    renamed files

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,25 @@
-enum Tour_type {
-    Leicht,
-    Medium,
-    Monster,
-    Zerstörer,
-}
-#[derive(PartialEq)]
-enum Person {
-    Mama,
-    Papa,
-    Jesko,
-    Jannis,
-}
+mod calculateData;
 
-struct Tour {
-    name: String,
-    kilometer: f32,
-    höhenmeter: i32,
-    tour_type: Option<Tour_type>,
-    teilnehmer: Vec<Person>,
-    schwierigkeitsBonus: i32,
+use macroquad::prelude::*;
+use macroquad::ui;
+
+fn get_config() -> Conf {
+    Conf { window_title: "touren tracker".to_string(), window_width: 1920, window_height: 1080, fullscreen: true, window_resizable: false, ..Default::default()}
 }
 
 
-fn writeToJSON(tour: &Tour){
 
-}
+#[macroquad::main(get_config)]
+async fn main() {
+    loop {
+        clear_background(GRAY);
 
-impl Tour {
-    fn new(name: &str, kilometer: f32, höhenmeter: i32, teilnehmer: Vec<Person>, schwierigkeitsBonus: i32) -> Tour{
-        let name = name.to_string();
-        let mut tour_type: Option<Tour_type> = None;
-        if höhenmeter >= 0 && höhenmeter <= 499 {
-            tour_type = Some(Tour_type::Leicht);
+
+        if is_key_pressed(KeyCode::Escape) {
+            break;
         }
-        if höhenmeter >= 500 && höhenmeter <= 999 {
-            tour_type = Some(Tour_type::Medium);
-        }
-        if höhenmeter >= 1000 && höhenmeter <= 1399 {
-            tour_type = Some(Tour_type::Monster);
-        }
-        if höhenmeter >= 1400{
-            tour_type = Some(Tour_type::Zerstörer);
-        }
-        
-        Tour {
-            name,
-            kilometer,
-            höhenmeter,
-            tour_type,
-            teilnehmer,
-            schwierigkeitsBonus,
-        }
+
+
+        next_frame().await;
     }
-
-    fn calculateMoney(&self, person: Person) -> i32{
-        let mut money = 0;
-        if self.teilnehmer.contains(&person) {
-            money += self.schwierigkeitsBonus;
-
-            // tour type money
-            match self.tour_type{
-                Some(Tour_type::Medium)=> money += 3,
-                Some(Tour_type::Monster) => money += 5,
-                Some(Tour_type::Zerstörer) => money += 10,
-                _ => println!("Tour type not found.."),
-            }
-    
-            if self.kilometer >= 10.0 {
-                money += 1;
-            }
-    
-            if self.kilometer >= 15.0 {
-                money += 1
-            }  
-        }
-
-        money   
-    }
-}
-
-
-
-
-fn main(){
-    let neueTour = Tour::new("test", 9.0, 500, vec![Person::Jannis, Person::Jesko], 0);
-    let money = neueTour.calculateMoney(Person::Jesko);
-
-    println!("{:?}", money);
 }
