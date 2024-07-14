930c53bd6a6e7f0fdc3aec8637a6323b53b95810
Jesko <jjputzi0410@gmail.com>
2024-07-14 19:45:30 +0200

    commit

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn get_config() -> Conf {
 #[macroquad::main(get_config)]
 async fn main() {
     loop {
-        clear_background(BLUE);
+        clear_background(BLACK);
 
 
         if is_key_pressed(KeyCode::Escape) {
